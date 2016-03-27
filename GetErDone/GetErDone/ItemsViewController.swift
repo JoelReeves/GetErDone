f1582e3f15e774e716e5f01202fd8ffa0647defb
Joel Reeves <jreeves@tonicdesign.com>
2016-03-27 10:10:10 -0400

    implementing method to set the table view's items

--- GetErDone/GetErDone/ItemsViewController.swift
+++ GetErDone/GetErDone/ItemsViewController.swift
@@ -15,4 +15,15 @@ class ItemsViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return toDoItemStore.allItems.count
     }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // creating instance of UITableView cell with a default appearance
+        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+        
+        // set the text on the cell
+        let item = toDoItemStore.allItems[indexPath.row]
+        cell.textLabel?.text = item.name
+        
+        return cell
+    }
 }
