e2b258399ec37b5154553572e5d3417907c3c145
Joel Reeves <jreeves@tonicdesign.com>
2016-03-27 09:30:46 -0400

    creating class to store an array of ToDoItems

---
+++ GetErDone/GetErDone/ToDoItemStore.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoItemStore.swift
+//  GetErDone
+//
+//  Created by Joel Reeves on 3/27/16.
+//  Copyright © 2016 Tonic Design. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItemStore {
+    
+    var allItems = [ToDoItem]()
+    
+    
+}
