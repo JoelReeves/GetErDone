d820316805664c86af6e3620c181874ba3d1c141
Joel Reeves <jreeves@tonicdesign.com>
2016-03-27 18:54:39 -0400

    created method to place 3 items into the table; created method to get the item name text in the table

--- GetErDone/GetErDone/ToDoItem.swift
+++ GetErDone/GetErDone/ToDoItem.swift
@@ -13,6 +13,12 @@ class ToDoItem {
     var allItems = [NSDictionary]()
     var firebaseDictionary = [String: NSDictionary]()
     
+    func createItems() {
+        createItem("Take over the world!!!", isComplete: false)
+        createItem("MVP with Adrienne", isComplete: false)
+        createItem("Kill myself b/c of the HATE", isComplete: false)
+    }
+    
     func createItem(name: String, isComplete: Bool) -> [String: NSDictionary] {
         struct Static {
             static var counter = 0
@@ -26,5 +32,15 @@ class ToDoItem {
         firebaseDictionary["Item \(Static.counter)"] = item
         
         return firebaseDictionary
+    }
+    
+    func getItemNameText(index: Int) -> String {
+        let item = allItems[index]
+        
+        if let name = item["name"] as? String {
+            return name
+        } else {
+            return "unknown"
+        }
     }
 }
