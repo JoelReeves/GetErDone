47a89c68046a7b4c0429e52878a705cdf919d0b8
Joel Reeves <jreeves@tonicdesign.com>
2016-03-28 10:18:53 -0400

    removing unnecessary code in FirebaseItem class

--- GetErDone/GetErDone/FirebaseItem.swift
+++ GetErDone/GetErDone/FirebaseItem.swift
@@ -9,16 +9,8 @@
 import UIKit
 
 class FirebaseItem {
-    
-    var allItems = [String]()
-    var itemsDictionary = [NSDictionary]()
+
     var firebaseDictionary = [String: NSDictionary]()
-    
-    func createItems() {
-        createItem("Take over the world!!!", isComplete: false)
-        createItem("MVP with Adrienne", isComplete: false)
-        createItem("Kill myself b/c of the HATE", isComplete: false)
-    }
     
     func createItem(name: String, isComplete: Bool) -> [String: NSDictionary] {
         struct Static {
@@ -29,23 +21,8 @@ class FirebaseItem {
         
         let item = ["name": name, "isComplete": isComplete]
         
-        itemsDictionary.append(item)
-        
-        let nameString = getItemNameText(itemsDictionary.count - 1)
-        allItems.append(nameString)
-        
         firebaseDictionary["Item \(Static.counter)"] = item
         
         return firebaseDictionary
-    }
-    
-    func getItemNameText(index: Int) -> String {
-        let item = itemsDictionary[index]
-        
-        if let name = item["name"] as? String {
-            return name
-        } else {
-            return "unknown"
-        }
     }
 }
