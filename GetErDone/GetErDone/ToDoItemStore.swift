b7897f196796b658cf46e3b0e42a7f2f7d553126
Joel Reeves <jreeves@tonicdesign.com>
2016-03-28 09:25:15 -0400

    creating ToDoItemStore class to manage an array of ToDoItems

---
+++ GetErDone/GetErDone/ToDoItemStore.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoItemStore.swift
+//  GetErDone
+//
+//  Created by Joel Reeves on 3/28/16.
+//  Copyright © 2016 Tonic Design. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItemStore {
+    
+    var allItems = [ToDoItem]()
+    
+    func createItem(name: String) -> ToDoItem {
+        let item = ToDoItem(itemName: name)
+        allItems.append(item)
+        return item
+    }
+    
+}
