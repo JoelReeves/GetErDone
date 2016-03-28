df40ced503661a2a716de3e76377b01760c81140
Joel Reeves <jreeves@tonicdesign.com>
2016-03-28 09:22:45 -0400

    creating ToDoItem class

--- GetErDone/GetErDone/ToDoItem.swift
+++ GetErDone/GetErDone/ToDoItem.swift
@@ -2,50 +2,24 @@
 //  ToDoItem.swift
 //  GetErDone
 //
-//  Created by Joel Reeves on 3/27/16.
+//  Created by Joel Reeves on 3/28/16.
 //  Copyright © 2016 Tonic Design. All rights reserved.
 //
 
 import UIKit
 
-class ToDoItem {
+class ToDoItem: NSObject {
+    var name: String
+    var isComplete: Bool
     
-    var allItems = [String]()
-    var itemsDictionary = [NSDictionary]()
-    var firebaseDictionary = [String: NSDictionary]()
-    
-    func createItems() {
-        createItem("Take over the world!!!", isComplete: false)
-        createItem("MVP with Adrienne", isComplete: false)
-        createItem("Kill myself b/c of the HATE", isComplete: false)
+    init(name: String, isComplete: Bool) {
+        self.name = name
+        self.isComplete = isComplete
+        
+        super.init()
     }
     
-    func createItem(name: String, isComplete: Bool) -> [String: NSDictionary] {
-        struct Static {
-            static var counter = 0
-        }
-        
-        Static.counter += 1
-        
-        let item = ["name": name, "isComplete": isComplete]
-        
-        itemsDictionary.append(item)
-        
-        let nameString = getItemNameText(itemsDictionary.count - 1)
-        allItems.append(nameString)
-        
-        firebaseDictionary["Item \(Static.counter)"] = item
-        
-        return firebaseDictionary
-    }
-    
-    func getItemNameText(index: Int) -> String {
-        let item = itemsDictionary[index]
-        
-        if let name = item["name"] as? String {
-            return name
-        } else {
-            return "unknown"
-        }
+    convenience init(itemName: String) {
+        self.init(name: itemName, isComplete: false)
     }
 }
