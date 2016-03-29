5299af2169f8f04133a8a67268fdf4015b190f02
Joel Reeves <jreeves@tonicdesign.com>
2016-03-29 11:36:28 -0400

    creating class for the table's cells

---
+++ GetErDone/GetErDone/ToDoItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItemCell.swift
+//  GetErDone
+//
+//  Created by Joel Reeves on 3/29/16.
+//  Copyright © 2016 Tonic Design. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItemCell: UITableViewCell{
+    
+}
